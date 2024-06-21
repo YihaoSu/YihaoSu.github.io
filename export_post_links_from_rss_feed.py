7cd47ca61a3c9c45e16118071dcdd9c739335794
YihaoSu <yhsu@astro.ncu.edu.tw>
2024-06-21 15:04:52 +0800

    Update blog and speaking page

--- export_post_links_from_rss_feed.py
+++ export_post_links_from_rss_feed.py
@@ -1,8 +1,6 @@
 import feedparser
 
-# feed_source = 'https://mirror.xyz/astrobackhacker.eth/feed/atom'
-feed_source = 'https://cloudflare-ipfs.com/ipns/astrobackhacker.eth/rss.xml'
-# feed_source = 'https://astrobackhacker.eth.limo/rss.xml'
+feed_source = 'https://4everland.io/ipns/astrobackhacker.eth/rss.xml'
 d = feedparser.parse(feed_source)
 entries = d['entries']
 
